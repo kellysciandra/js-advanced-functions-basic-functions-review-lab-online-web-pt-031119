a0379043c0685fab9c9d9c227fe1ac1594ed6c94
Kelly Sciandra <kellysciandradev@icloud.com>
2020-01-23 20:22:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,13 +17,13 @@ const  Calculator = {
   add(a,b) {
     return a + b
   }
-  subtract(a,b) {
-  //   return a - b
-  // }
-  multiply(a,b) {
-  //   return a * b 
-  // }
-  divide(a,b) {
-  //   return a / b
-  // }
+  // subtract(a,b) {
+  // //   return a - b
+  // // }
+  // multiply(a,b) {
+  // //   return a * b 
+  // // }
+  // divide(a,b) {
+  // //   return a / b
+  // // }
 }
