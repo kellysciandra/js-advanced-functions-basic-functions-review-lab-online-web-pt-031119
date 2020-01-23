a63a57aab181e4de4df47fa11fc80eb752ca5513
Kelly Sciandra <kellysciandradev@icloud.com>
2020-01-23 20:12:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function mondayWork(activity="go to the office") {
 
 function wrapAdjective(flair="*") {
  return function(adjective='special') {
-    return `You are ${special}`
+    return `You are ${flair}${special}${flair}`
   }
 }
